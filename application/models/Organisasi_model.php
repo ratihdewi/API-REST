e2606c00bb01a930b3ef648dbc5bb22ffedba1ee
Ratih Kusuma Dewi <ratihkusumadewi09@hmail.com>
2021-12-03 13:01:23 +0700

    position-

--- application/models/Organisasi_model.php
+++ application/models/Organisasi_model.php
@@ -11,7 +11,7 @@ class Organisasi_model extends CI_Model
 
             return $this->db->get()->result_array();
         } else{
-            $this->db->select('u.name u.nip, u.sso_username, jabatan.nama_jabatan, jabatan.id,, unit.nama_unit_kerja, unit.id as unit, jabatan.parent_id');
+            $this->db->select('u.name, u.nip, u.sso_username, jabatan.nama_jabatan, jabatan.id, unit.nama_unit_kerja, unit.id as unit, jabatan.parent_id');
             $this->db->from('users as u');
             $this->db->join('jabatans as jabatan', 'u.jabatan_id = jabatan.id');
             $this->db->join('unit_kerjas as unit', 'unit.id = jabatan.unit_kerja_id');
